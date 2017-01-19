b32350086def4500f0a8a77b11251867547db4c5
jsvirzi <jsvirzi@gmail.com>
2017-01-19 13:05:44 -0800

    script for setting up

---
+++ share/setup.sh
@@ -0,0 +1,6 @@
+tgtDirectory="/storage/emulated/0/Download/" 
+adb push share/beep.mp3 ${tgtDirectory}
+adb push share/may-i-have-your-attention.mp3 ${tgtDirectory}
+adb push share/no-trespassing.mp3 ${tgtDirectory}
+adb push share/tiny-bell.mp3 ${tgtDirectory}
+
